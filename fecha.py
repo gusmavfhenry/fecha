3f809e5f6a45e444d2c2ca54bf46eee1cdad3bf7
gusmavfhenry <86271318+gusmavfhenry@users.noreply.github.com>
2021-07-18 22:02:01 -0300

    Create fecha.py

---
+++ fecha.py
@@ -0,0 +1,31 @@
+# -*- coding: utf-8 -*-         
+
+#--------------------------------------------------------------------------
+
+def calcularAnios(sFechaInicial, sFechaFinal):
+	nAnioAuxiliar = 0
+
+	nDiaInicio, nMesInicio, nAnioInicio = (int(item) for item in sFechaInicial.split("/"))
+
+	nDiaFinal, nMesFinal, nAnioFinal = (int(item) for item in sFechaFinal.split("/"))
+
+	nAnioAuxiliar = nAnioFinal - nAnioInicio
+
+	if (nMesFinal < nMesInicio) or (nDiaFinal < nDiaInicio):
+		nAnioAuxiliar = nAnioAuxiliar - 1
+
+	return nAnioAuxiliar
+
+#--------------------------------------------------------------------------
+
+def main():
+
+	sFechaInicial = input("Fecha de inicio (DD/MM/AAAA): ")
+	sFechaFinal = input("Fecha final (DD/MM/AAAA): ")
+	
+	print ("Cantidad de años: {}".format(calcularAnios(sFechaInicial, sFechaFinal)))
+
+#--------------------------------------------------------------------------
+
+if __name__ == '__main__':
+	main()
